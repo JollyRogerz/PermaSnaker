ef6faf92cfcfe433b7e2665c2037c5ab9a89bf03
Jolly_Roger <voyagercoinz@gmail.com>
2019-06-26 22:38:19 +0100

    LoginFeature

--- src/login.js
+++ src/login.js
@@ -4,6 +4,9 @@ function login(files) {
     fr.onload = function (ev) {
         try {
             const wallet = JSON.parse(ev.target.result);
+            arweave.wallets.jwkToAddress(wallet).then((address) => {
+                console.log(`Arweave wallet address: ${address}`)
+            });
 
         } catch (err) {
             console.log('Error logging in: ', err);
